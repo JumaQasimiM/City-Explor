9a45ed8ec6a18ab98857e1ad498118d55baa753e
M Juma <qasimimohammad819@gmail.com>
2026-01-10 00:11:27 +0100

    frontend: create category page and categories hook

--- frontend/src/hooks/useCategories.js
+++ frontend/src/hooks/useCategories.js
@@ -1,5 +1,6 @@
 import { useFetch } from "./useFetch";
 import { ApiUrl } from "../api/ApiUrl";
+import { useState } from "react";
 
 /**
  * custom Hook to fetch Categories from api
@@ -16,11 +17,81 @@ import { ApiUrl } from "../api/ApiUrl";
 
 // get all categories
 export const useCategories = () => {
-  const { data = [], error, loading } = useFetch(`${ApiUrl}/categories`);
+  const {
+    data = [],
+    error,
+    loading,
+    refetch,
+  } = useFetch(`${ApiUrl}/categories`);
   return {
     categories: data,
     error,
     loading,
-    hasCategory: data.lenght > 0,
+    hasCategory: data.length > 0,
+    refetch,
   };
+};
+
+// create new category
+
+export const useCreateCategory = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const createCategory = async (payload) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const res = await fetch(`${ApiUrl}/categories`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+      });
+
+      if (!res.ok) {
+        const text = await res.text();
+        throw new Error(text || "Failed to create category");
+      }
+
+      const data = await res.json();
+      return data;
+    } catch (err) {
+      const message = err.message || "Something went wrong";
+      setError(message);
+      throw new Error(message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { createCategory, loading, error };
+};
+
+// delete category
+
+export const useDeleteCategory = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const deleteCategory = async (category_id) => {
+    setLoading(true);
+    try {
+      const res = await fetch(`${ApiUrl}/categories/${category_id}`, {
+        method: "DELETE",
+      });
+      if (!res.ok) {
+        const text = await res.text();
+        throw new Error(text || "Failed to create category");
+      }
+      await res.json();
+      return true;
+    } catch (error) {
+      setError(error.message);
+      throw new Error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { deleteCategory, error, loading };
 };
---
+++ frontend/src/pages/dashbord/Category.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import { IoMdAdd } from "react-icons/io";
+import {
+  useCategories,
+  useCreateCategory,
+  useDeleteCategory,
+} from "../../hooks/useCategories";
+import { toast } from "react-toastify";
+
+export const Category = () => {
+  const [categoryName, setCategoryName] = useState("");
+
+  const { categories, refetch } = useCategories();
+  const { createCategory, loading } = useCreateCategory();
+  const { deleteCategory } = useDeleteCategory();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!categoryName.trim()) return toast.error("Category is required");
+
+    try {
+      await createCategory({ name: categoryName });
+      toast.success("Category created successfully");
+      setCategoryName("");
+      refetch();
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+  //   delete
+  const handleDelete = async (cate_id) => {
+    try {
+      await deleteCategory(cate_id);
+      refetch();
+      toast.success("Category deleted successfully");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <section className="my-4 p-4 bg-gray-900 rounded-lg shadow-md">
+      <h1 className="text-xl font-semibold mb-4">Categories</h1>
+
+      {/* Form to add category */}
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col md:flex-row md:items-center gap-3 mb-6"
+      >
+        <input
+          type="text"
+          placeholder="Enter Category"
+          value={categoryName}
+          onChange={(e) => setCategoryName(e.target.value)}
+          className="flex-1 py-2 px-3 rounded border border-gray-600 focus:ring-2 focus:ring-green-500 focus:outline-none"
+        />
+        <button
+          type="submit"
+          disabled={loading}
+          className="flex gap-2 items-center text-center bg-sky-700 py-2 px-4 rounded hover:bg-sky-600 disabled:opacity-50"
+        >
+          <IoMdAdd size={20} /> Add
+        </button>
+      </form>
+
+      {/* Table with horizontal scroll */}
+      <div className="overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-700 table-auto">
+          <thead className="bg-gray-800 text-white/70 ">
+            <tr>
+              <th className="px-4 py-2 text-left">#</th>
+              <th className="px-4 py-2 text-left">Name</th>
+              <th className="px-4 py-2 text-left">Places</th>
+              <th className="px-4 py-2 text-left">Actions</th>
+            </tr>
+          </thead>
+
+          <tbody className="bg-gray-800 divide-y divide-gray-700 text-white/80">
+            {categories.length === 0 ? (
+              <tr>
+                <td colSpan={5} className="text-center py-4 text-gray-400">
+                  No categories found
+                </td>
+              </tr>
+            ) : (
+              categories.map((cate, idx) => (
+                <tr key={cate.id} className="hover:bg-gray-700">
+                  <td className="px-4 py-2">{idx + 1}</td>
+                  <td className="px-4 py-2">{cate.name}</td>
+                  <td className="px-4 py-2">5</td>
+                  <td className="px-4 py-2 flex gap-3 text-sm">
+                    <button className="text-blue-400 hover:underline cursor-pointer">
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => handleDelete(cate.id)}
+                      className="text-red-500 hover:underline cursor-pointer"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
