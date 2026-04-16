e193fa8c232804ebbb8d66a80b0ce9445d9f44c5
M Juma <qasimimohammad819@gmail.com>
2026-04-16 19:26:28 +0200

    update api url and add the DRF server url for api

--- frontend/src/api/ApiUrl.js
+++ frontend/src/api/ApiUrl.js
@@ -2,7 +2,8 @@ let ApiUrl = "";
 
 if (import.meta.env.MODE === "development") {
   // json-server in Entwicklung
-  ApiUrl = "http://localhost:3000";
+  // ApiUrl = "http://localhost:3000";http://localhost:8000/api/
+  ApiUrl = "http://localhost:8000/api";
 } else {
   // Produktiv-Backend
   ApiUrl = "https://city-api-icko.onrender.com";
