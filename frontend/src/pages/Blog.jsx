c21b187123327eb9020e41ac61633a46c0379d79
M Juma <qasimimohammad819@gmail.com>
2026-07-08 21:34:55 +0200

    frontend: update blog.jsx page

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -30,7 +30,7 @@ export const Blog = () => {
 
   return (
     <section className="bg-gray-50 dark:bg-slate-900 ">
-      <div className="max-w-8xl relative h-[220px] md:h-[400px] overflow-hidden">
+      <div className="max-w-8xl relative h-[230px] md:h-[400px] overflow-hidden">
         {/* ================= HERO ================= */}
         <div className="">
           <img
@@ -44,7 +44,7 @@ export const Blog = () => {
 
           {/* content */}
           <div className="max-w-7xl mx-auto absolute inset-0 flex flex-col justify-center px-6  md:px-12 ">
-            <span className="text-green-400 text-xs uppercase tracking-widest mb-2 mt-10">
+            <span className="text-green-400 text-xs uppercase tracking-widest mb-2 mt-15">
               Travel Blog
             </span>
 
@@ -61,9 +61,9 @@ export const Blog = () => {
       </div>
       <div className="max-w-7xl mx-auto px-4 space-y-12 pt-10 py-10">
         {/* ================= CONTENT ================= */}
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-10">
           {/* ===== MAIN BLOG LIST ===== */}
-          <article className="lg:col-span-2 space-y-8">
+          <article className="lg:col-span-3 space-y-8">
             {blogs.map((blog) => (
               <BlogCard blog={blog} key={blog.id} />
             ))}
@@ -75,7 +75,7 @@ export const Blog = () => {
               h-fit sticky top-24
               bg-white dark:bg-slate-900
               border border-gray-200 dark:border-slate-700
-              rounded-2xl p-6 space-y-6 shadow-sm"
+              rounded-xl p-6 space-y-6 shadow-sm"
           >
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
               Popular Blogs
